ee36bb5458084a7bc91538201cd683fd82b5d8f6
Giacomo Loparco <loparcog@mcmaster.ca>
2019-06-20 19:09:04 -0400

    Push to RPI

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-//DeLorean Discord Bot
+//Tea Discord Bot
 //Giacomo Loparco | Last Edited 01/09/19
 
 const botSettings = require("./botsettings.json");
@@ -12,7 +12,8 @@ const client = new discord.Client();
 const prefix = botSettings.prefix;
 
 client.on("ready", async () => {
-	console.log("Delorean ONLINE");
+	uptime = new Date()
+	console.log("Tea Bot ONLINE @\n" + uptime);
 	client.user.setPresence({
 		game: {name: "Facebook Story | ./help", 
 			type: 'LISTENING' },
@@ -53,7 +54,7 @@ client.on("message", async message => {
 		let embed = new discord.RichEmbed()
 			.setAuthor("User Info", message.author.avatarURL)
 			.setDescription(`Requested by ${message.author.username}`)
-			.setColor("98FB98")
+			.setColor("D0F0C0")
 			.addField("Full Username", `${user.username}#${user.discriminator}`, true)
 			.addField("User ID", user.id, true)
 			.addField("Account Created", user.createdAt)
@@ -71,7 +72,7 @@ client.on("message", async message => {
 		let embed = new discord.RichEmbed()
 			.setAuthor("Server Info", message.author.avatarURL)
 			.setDescription(`Requested by ${message.author.username}`)
-			.setColor("98FB98")
+			.setColor("D0F0C0")
 			.addField("Server Name", server.name, true)
 			.addField("Server ID", server.id, true)
 			.addField("Owner", `${server.owner.user.username}#${server.owner.user.discriminator}`, true)
@@ -93,6 +94,36 @@ client.on("message", async message => {
 				msg.delete(10000);
 			});
 		return;
+	}
+
+	else if(command === `${prefix}prog`){
+		var rawdata = fs.readFileSync('sinfo.json');
+		var database = JSON.parse(rawdata);
+		var roles = database[message.guild.id].roles
+		let dfinal = JSON.stringify(database);
+		fs.writeFileSync('sinfo.json', dfinal);
+		if(args.length == 0){
+			if (roles.length == 0){
+				message.channel.send("No roles found!");
+				console.log(message.guild.roles.array());
+				// Show possible roles
+			}
+			else{
+				// Add/Subtract a role (check for admin)
+				// Type specific role to add to user
+				rolestr = ""
+				for (i = 0; i < roles.length; i++){
+					rolestr += roles[i] + ", ";
+				}
+				let embed = new discord.RichEmbed()
+				.setAuthor("Program Roles", message.author.avatarURL)
+				.setDescription(rolestr)
+				.setColor("D0F0C0")
+				.setTimestamp(Date.now());
+
+				message.channel.send(embed);
+			}
+		}
 	}
 
 	else if(command === `${prefix}weather` || command === `${prefix}w`){
@@ -118,7 +149,7 @@ client.on("message", async message => {
 				.setDescription(`**${current.skytext}**`)
 				.setAuthor(`Weather for ${current.observationpoint}`)
 				.setThumbnail(current.imageUrl)
-				.setColor("98FB98")
+				.setColor("D0F0C0")
 				.addField('Timezone', `UTC ${location.timezone}`, true)
 				.addField('Temperature (F)', `${Math.round((current.temperature * 1.8) + 32)} F`, true)
 				.addField('Temperature (˚C)', `${current.temperature}˚C`, true)
@@ -137,7 +168,7 @@ client.on("message", async message => {
 		let embed = new discord.RichEmbed()
 			.setAuthor("Bot Commands", client.user.avatarURL)
 			.setDescription(`Use the prefix ./ before all commands, all <arguments> are optional`)
-			.setColor("98FB98")
+			.setColor("D0F0C0")
 			.addField("calendar", "get the calendar for the current month")
 			.addField("weather <city>", "get the weather at a given location")
 			.addField("ping", "check bot ping")
@@ -155,25 +186,35 @@ client.on("message", async message => {
 			return;
 		}
 		else{
-			currdate = new Date()
+			currdate = new Date();
+			givenmonth = parseInt(args[0]);
+			if(!Number.isInteger(givenmonth) || 1 > givenmonth || 12 < givenmonth){
+					message.channel.send("Invalid month argument!");
+					return;
+			}
 			if(args[1] == undefined){
 				message.channel.send(customMF(givenmonth, currdate.getFullYear()));
 				return;
 			}
 			else{
-				givenyear = args[1];
-				message.channel.send(customMF(givenmonth, givenyear))
+				givenyear = parseInt(args[1]);
+				if(!Number.isInteger(givenyear) || 1900 > givenyear || 2100 < givenyear){
+					message.channel.send("Invalid year argument!");
+					return;
+				}
+				message.channel.send(customMF(givenmonth, givenyear));
+				return;
 			}
 		}
 	}
-
+/*
 	else if(command === `${prefix}task` || command === `${prefix}t`){
 		if (args.length == 0 || args[0] === 'help'){
 			let embed = new discord.RichEmbed()
 				.setAuthor("Task Commands", client.user.avatarURL)
 				.setDescription(`All possible arguments for the ./task and ./t command\n` +
 					'NOTE: You cannot start your task text with a number, but you can with quotes!')
-				.setColor("98FB98")
+				.setColor("D0F0C0")
 				.addField("add dd mm yy <text>", "add a task to a specific date (if no year/month, next possible day)")
 				.addField("get dd mm yy", "get task from a given day (or all from current month)")
 				.addField("del dd mm yy", "delete a task you set for a given date")
@@ -221,19 +262,48 @@ client.on("message", async message => {
 		}
 		return;
 	}
+*/
 
 	else if(command === `${prefix}init`){
 		//Personal, use to track changes
 		if(message.author.id == `196388136656437250`){
-			var rawdata = fs.readFileSync('tasks.json');
+			var rawdata = fs.readFileSync('sinfo.json');
 			var database = JSON.parse(rawdata);
-			database[message.guild.id] = {};
+			database[message.guild.id].roles = [];
 			let dfinal = JSON.stringify(database);
-			fs.writeFileSync('tasks.json', dfinal);
+			fs.writeFileSync('sinfo.json', dfinal);
+			console.log("Initialized!")
 		}
 		return;
 	}
-	return;
+
+	else if(command === `${prefix}rules`){
+		if(message.author.id == `196388136656437250`){
+			let embed = new discord.RichEmbed()
+				.setAuthor("Welcome to McMaster MOOD", message.guild.iconURL)
+				.setDescription("Have a good time and please,")
+				.setColor("D0F0C0")
+				.addField("1. BE RESPECTFUL!", 
+					"This is a music discussion forum and so disagreement is to be expected, but keep it civil."+
+					" If you're having a problem with a user, don't hesitate to PM one of the execs")
+				.addField("2. Keep discussion relevant to the channel its in.",
+					"Keeping channels nice and neat helps to keep all channels running smoothly")
+				.addField("3. No NSFW content outside of #moodposting.",
+					"Keep the naughties at bay")
+				.addField("4. Feel free to invite anyone you think would be a good fit for the community.",
+					"Non-Mac people included!")
+				.addField("5. If you're new, use the `./prog <role>` command in #welcome to give yourself a faculty role so that you can post in other channels.",
+					"Use `./prog` to see all possible roles")
+				.addField("6. Please limit submissions of albums in #weekly-submissions to 5 per week.",
+					"Weekly playlists will be picked randomly from all given albums")
+				.setFooter("MOOD: Mid Ohio Open Disk-Golf");
+			message.channel.send(embed)
+				.then(msg => {
+					msg.channel.send("https://discord.gg/FpJzvhH");
+				})
+		}
+	}
+
 });
 
 
@@ -248,7 +318,7 @@ function MonthFormat(day, curr){
               1    2    3    4    5
     6    7    8    9    10   11   12
     13   14!  15!  16!  17   18   19
-    20   21   22   23   24   25   26
+    20   21   22   23<  24   25   26
     27   28   29   30   31
 	*/
 	LY = false;
@@ -281,17 +351,22 @@ function MonthFormat(day, curr){
 			dotw = 0;
 			cal += "\n";
 		}
+		cal += (i);
 		if (i < 10){
-			cal += (i + " ");
+			if (i == daynow){
+				cal += "<   "
+			}
+			else{
+				cal += "    "
+			}
 		}
 		else{
-			cal += (i);
-		}
-		if(i == daynow){
-			cal += "<  "
-		}
-		else{
-			cal += "   "
+			if(i == daynow){
+				cal += "<  "
+			}
+			else{
+				cal += "   "
+			}
 		}
 		dotw++;
 		i++;
@@ -306,7 +381,7 @@ function currentMF(){
 }
 
 function customMF(month, year){
-	newmo = month + 1;
+	newmo = month - 1;
 	day = new Date(year, newmo);
 	return MonthFormat(day, false);
 }
